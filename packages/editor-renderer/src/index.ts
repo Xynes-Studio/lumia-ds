24f7f69dd4b535b519f559e15641cb7d7683ce62
archan96 <archan.ray2011@gmail.com>
2025-12-07 22:08:19 +0530

    feat: Introduce extensible block registry for `LumiaDocument` and enhance table rendering with new testing infrastructure."

--- packages/editor-renderer/src/index.ts
+++ packages/editor-renderer/src/index.ts
@@ -3,3 +3,4 @@ export {
   type LumiaDocumentProps,
 } from './components/LumiaDocument';
 export { type LumiaEditorStateJSON, type BlockDefinition } from './types';
+export { defaultBlockRegistry } from './blockRegistry';
